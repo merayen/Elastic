b170e7f8bec3e34f8e8231083cb44bd9d858c798
Merayen <pigzelner@gmail.com>
2015-06-17 19:49:44 +0200

    Preparing to implement glue nodes

---
+++ src/net/merayen/merasynth/glue/GlueNode.java
@@ -0,0 +1,58 @@
+package net.merayen.merasynth.glue;
+
+import org.json.simple.JSONObject;
+
+public abstract class GlueNode {
+	/*
+	 * Glue nodes that contains a netlist Node and an UI Node. These nodes are
+	 * responsible for controlling both types and glue them together. Currently
+	 * we want those two different node systems (UI and netlist) completely
+	 * separated.
+	 */
+	
+	protected net.merayen.merasynth.netlist.Node net_node;
+	protected net.merayen.merasynth.ui.objects.node.Node ui_node;
+	
+	public GlueNode(net.merayen.merasynth.netlist.Node net_node, net.merayen.merasynth.ui.objects.node.Node ui_node) {
+		this.net_node = net_node;
+		this.ui_node = ui_node;
+		onCreate();
+	}
+	
+	public void doInit() {
+		onInit();
+	}
+	
+	public JSONObject doDump() {
+		JSONObject obj = onDump();
+		
+		// TODO do anything with it?
+		
+		return obj;
+	}
+	
+	protected void onCreate() {
+		/*
+		 * Called when created for the first time
+		 */
+	}
+	
+	protected void onInit() {
+		/*
+		 * Called when initialized (after onCreate or before onRestore)
+		 */
+	}
+	
+	protected void onRestore() {
+		/*
+		 * Called when restored from JSON
+		 */
+	}
+	
+	protected JSONObject onDump() { // TODO return JSON object. Really?
+		/*
+		 * Called when dumping to JSON
+		 */
+		return new JSONObject();
+	}
+}
---
+++ src/net/merayen/merasynth/glue/nodes/PulseGenerator.java
@@ -0,0 +1,13 @@
+package net.merayen.merasynth.glue.nodes;
+
+import net.merayen.merasynth.glue.GlueNode;
+import net.merayen.merasynth.netlist.Node;
+
+public class PulseGenerator extends GlueNode {
+
+	public PulseGenerator(Node net_node, // TODO
+			net.merayen.merasynth.ui.objects.node.Node ui_node) {
+		super(net_node, ui_node);
+	}
+
+}
