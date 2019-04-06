239bc7dcdf8f6b38f09cd72e081aed128626de68
Merayen Salberg <pigzelner@gmail.com>
2019-04-06 16:22:11 +0200

    Implemented unnecessary grid in NodeView

--- src/net/merayen/elastic/ui/objects/top/views/nodeview/NodeView.kt
+++ src/net/merayen/elastic/ui/objects/top/views/nodeview/NodeView.kt
@@ -80,6 +80,20 @@ class NodeView : View() {
 
 		draw.fillRect(2f, 2f, getWidth() - 4, getHeight() - 4)
 
+
+		// Silly, stupid grid because why not
+		draw.setStroke(1f)
+		draw.setColor(100, 100, 100)
+
+		for (i in 0 until layoutWidth.toInt() + 50 step 50) {
+			val u = i + container.translation.x % 50
+			draw.line(u, 0f, u, layoutHeight)
+		}
+		for (i in 0 until layoutHeight.toInt() + 50 step 50) {
+			val u = i + container.translation.y % 50
+			draw.line(0f, u, layoutWidth, u)
+		}
+
 		nodeViewBar.layoutWidth = getWidth()
 	}
 
