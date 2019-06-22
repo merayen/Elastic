4a4afb9631a0c57daea73c88b95ce0decb1aa354
Merayen Salberg <pigzelner@gmail.com>
2019-06-22 09:44:47 +0200

    Gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,58 +7,58 @@ import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
  */
 
 plugins {
-    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
-    id("org.jetbrains.kotlin.jvm").version("1.3.21")
+	// Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
+	id("org.jetbrains.kotlin.jvm").version("1.3.21")
 
-    // Apply the application plugin to add support for building a CLI application.
-    application
+	// Apply the application plugin to add support for building a CLI application.
+	application
 
 
 }
 
 repositories {
-    // Use jcenter for resolving your dependencies.
-    // You can declare any Maven/Ivy/file repository here.
-    jcenter()
+	// Use jcenter for resolving your dependencies.
+	// You can declare any Maven/Ivy/file repository here.
+	jcenter()
 }
 
 dependencies {
-    // Use the Kotlin JDK 8 standard library.
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	// Use the Kotlin JDK 8 standard library.
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
 
-    // Use the Kotlin test library.
-    testImplementation("org.jetbrains.kotlin:kotlin-test")
+	// Use the Kotlin test library.
+	testImplementation("org.jetbrains.kotlin:kotlin-test")
 
-    // Use the Kotlin JUnit integration.
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+	// Use the Kotlin JUnit integration.
+	testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
 
-    implementation("com.googlecode.json-simple:json-simple:1.1.1")
+	implementation("com.googlecode.json-simple:json-simple:1.1.1")
 
-    implementation("com.google.code.gson:gson:2.8.5")
+	implementation("com.google.code.gson:gson:2.8.5")
 }
 
 application {
-    // Define the main class for the application.
-    mainClassName = "net.merayen.elastic.Test"
+	// Define the main class for the application.
+	mainClassName = "net.merayen.elastic.Test"
 }
 
 sourceSets["main"].java.srcDirs(arrayOf("src/main/kotlin"))
 sourceSets["main"].withConvention(KotlinSourceSet::class) {
-    kotlin.srcDirs(arrayOf("src/main/kotlin"))
+	kotlin.srcDirs(arrayOf("src/main/kotlin"))
 }
 sourceSets["main"].resources.srcDirs(arrayOf("src/main/resources"))
 
 tasks.jar {
-    manifest {
-        attributes(
-                "Implementation-Title" to "Elastic",
-                "Main-Class" to "net.merayen.elastic.Main"
-        )
-    }
+	manifest {
+		attributes(
+			"Implementation-Title" to "Elastic",
+			"Main-Class" to "net.merayen.elastic.Main"
+		)
+	}
 
-    from(Callable {
-        configurations["compileClasspath"].map { if (it.isDirectory) it else zipTree(it) }
-    })
+	from(Callable {
+		configurations["compileClasspath"].map { if (it.isDirectory) it else zipTree(it) }
+	})
 }
--- src/main/kotlin/net/merayen/elastic/Test.kt
+++ src/main/kotlin/net/merayen/elastic/Test.kt
@@ -12,26 +12,26 @@ class Test {
 			//net.merayen.elastic.backend.context.Test.test();
 
 			// Works
-			//net.merayen.elastic.backend.architectures.local.Test.test();
-			//net.merayen.elastic.util.Test.test();
+			/*net.merayen.elastic.backend.architectures.local.Test.test();
+			net.merayen.elastic.util.Test.test();
 
-			//net.merayen.elastic.netlist.Test.test();
-			//net.merayen.elastic.system.intercom.Test.test();
+			net.merayen.elastic.netlist.Test.test();
+			net.merayen.elastic.system.intercom.Test.test();
 
-			//net.merayen.elastic.backend.analyzer.Test.test();
-			//net.merayen.elastic.backend.interfacing.Test.test();
-			//net.merayen.elastic.backend.mix.Test.test();
-			//net.merayen.elastic.ui.objects.top.viewport.Test.test();
+			net.merayen.elastic.backend.analyzer.Test.test();
+			net.merayen.elastic.backend.interfacing.Test.test();
+			net.merayen.elastic.backend.mix.Test.test();
+			net.merayen.elastic.ui.objects.top.viewport.Test.test();*/
 
 			//net.merayen.elastic.backend.architectures.local.nodes.delay_1.Delay.test();
 
-			//net.merayen.elastic.ui.Test.test();
+			/*net.merayen.elastic.ui.Test.test();
 
-			//net.merayen.elastic.backend.queue.TestKt.test();
+			net.merayen.elastic.backend.queue.test();
 
-			net.merayen.elastic.system.Test.test()
+			net.merayen.elastic.system.Test.test()*/
 
-			//println(net.merayen.elastic.system.Test.test())
+			println(net.merayen.elastic.system.Test.test())
 
 			println("Test done")
 		}
