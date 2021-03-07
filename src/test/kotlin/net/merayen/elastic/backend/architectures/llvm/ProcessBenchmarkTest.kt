96425bc68a283a92146c35d9ec6f5466aa316388
Einar Rosvoll <pigzelner@gmail.com>
2021-03-07 20:01:15 +0100

    Test the output value of the benchmark

--- src/test/kotlin/net/merayen/elastic/backend/architectures/llvm/ProcessBenchmarkTest.kt
+++ src/test/kotlin/net/merayen/elastic/backend/architectures/llvm/ProcessBenchmarkTest.kt
@@ -1,5 +1,6 @@
 package net.merayen.elastic.backend.architectures.llvm
 
+import net.merayen.elastic.backend.logicnodes.list.output_1.Output1NodeOutputData
 import net.merayen.elastic.system.intercom.ProcessRequestMessage
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
@@ -18,11 +19,16 @@ internal class ProcessBenchmarkTest {
 		while (t > System.currentTimeMillis()) {
 			supervisor.ingoing.send(ProcessRequestMessage())
 			supervisor.onUpdate()
-			while(supervisor.outgoing.isEmpty())
+			while (supervisor.outgoing.isEmpty())
 				Thread.sleep(0)
 
 			val messages = supervisor.outgoing.receiveAll()
 			assertEquals(1, messages.size)
+			for (message in messages) {
+				if (message is Output1NodeOutputData)
+					for (sample in message.audio.first()!!.iterator())
+						assertEquals(3.0f, sample)
+			}
 			framesProcessed++
 		}
 
