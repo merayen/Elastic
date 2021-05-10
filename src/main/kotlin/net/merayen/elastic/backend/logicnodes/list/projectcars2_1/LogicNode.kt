a57257f3a9c1bc77ce2c6a356a408c070ebc0ffd
Einar Rosvoll <pigzelner@gmail.com>
2021-05-10 21:00:30 +0200

    WIP on receiving UDP data from Project Cars 2

--- src/main/kotlin/net/merayen/elastic/backend/logicnodes/list/projectcars2_1/LogicNode.kt
+++ src/main/kotlin/net/merayen/elastic/backend/logicnodes/list/projectcars2_1/LogicNode.kt
@@ -3,9 +3,27 @@ package net.merayen.elastic.backend.logicnodes.list.projectcars2_1
 import net.merayen.elastic.backend.logicnodes.Format
 import net.merayen.elastic.backend.nodes.BaseLogicNode
 import net.merayen.elastic.backend.nodes.BaseNodeProperties
+import net.merayen.elastic.system.intercom.InputFrameData
 import net.merayen.elastic.system.intercom.NodeDataMessage
+import java.net.DatagramPacket
+import java.net.DatagramSocket
 
 class LogicNode : BaseLogicNode() {
+	class UDPDataReceiver private constructor() {
+		val socket = DatagramSocket(5606)
+		private val buffer = ByteArray(1024)
+
+		fun fetch(): DatagramPacket {
+			val packet = DatagramPacket(buffer, buffer.size)
+			socket.receive(packet)
+			return packet
+		}
+
+		companion object {
+			var instance = UDPDataReceiver()
+		}
+	}
+
 	override fun onInit() {
 		createOutputPort("rpm", Format.SIGNAL)
 		createOutputPort("nm", Format.SIGNAL)
@@ -15,10 +33,17 @@ class LogicNode : BaseLogicNode() {
 		createOutputPort("throttle", Format.SIGNAL)
 		createOutputPort("break", Format.SIGNAL)
 		createOutputPort("clutch", Format.SIGNAL)
+
+		println(UDPDataReceiver.instance)
 	}
 
 	override fun onParameterChange(instance: BaseNodeProperties?) {
 		updateProperties(instance)
+	}
+
+	override fun onPrepareFrame(): InputFrameData {
+		UDPDataReceiver.instance.fetch()
+		return super.onPrepareFrame()
 	}
 
 	override fun onData(data: NodeDataMessage?) {}
